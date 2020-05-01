552dfb1d4f62a7d5e7310f491ff52f9cdf5e720e
joao_vitor_gatti <joaogatti17@gmail.com>
2020-05-01 17:48:07 -0300

    comentarios na heap

--- BinaryHeap/src/Heap.java
+++ BinaryHeap/src/Heap.java
@@ -1,8 +1,16 @@
+/*
+Implementacao maxHeap de um heap binario.
+Um heap pode ser visto como um arvore binaria completa onde (maxheap) cada
+no eh maior que os seus filhos e assim recursivamente.Logo nesse caso
+a raiz eh o maior elemento.
+*/
+
 public class Heap {
 
     int[] heap;
     //maior tamanho possivel do array
     int maxsize;
+    //tamanho do array conforme adiciona-se elementos
     int size;
 
 
@@ -10,19 +18,25 @@ public class Heap {
         this.maxsize = maxsize;
         heap = new int[this.maxsize + 1];
         this.size = 0;
+        //estamos comecando o array no index 1 para simplificar
         heap[0] = Integer.MAX_VALUE;
     }
 
+    //retorna a posicao do pai do no da posicao position
     private int parent(int position){
         return (position / 2);
     }
+    //retorna a posicao do filho esquerdo do no da posicao position
     private int leftChild(int position){
         return (2*position);
     }
+    //retorna a posicao do filho direito do no da posicao position
     private int rightChild(int position){
         return ((2*position) + 1);
     }
 
+    //Esse metodo troca de valores dois nos, onde o no de maior valor
+    //vai antes do de menor valor
     private void swap(int firstPosition,int secondPosition){
         int temp;
         temp = heap[firstPosition];
@@ -30,6 +44,10 @@ public class Heap {
         heap[secondPosition] = temp;
     }
 
+
+    //esse metodo eh responsavel por "balancear" a arvore,isto e,
+    //dado um no ele ve se eh maior do q seus filhos,em caso negativo
+    //realiza as operacoes apropriadas para se "balancear"
     private void maxHeapify(int position){
         int largest;
         int leftSon = leftChild(position);
@@ -48,6 +66,7 @@ public class Heap {
         }
     }
 
+    //metodo para adicionar um elemento
     public void insert(int element){
         heap[++size] = element;
         int current = size;
@@ -57,6 +76,8 @@ public class Heap {
         }
     }
 
+    //responsavel por retornar o maior valor,
+    //ou seja,a raiz heap[1]
     public int extractMax(){
         int popped = heap[1];
         heap[1] = heap[size--];
