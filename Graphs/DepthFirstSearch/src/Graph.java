a018c591c3b5733873717d99de9306cdc8a522b5
joaovgatti <joaogatti17@gmail.com>
2020-06-10 10:46:17 -0300

    Graphs/DepthFirstSearch

--- Graphs/DepthFirstSearch/src/Graph.java
+++ Graphs/DepthFirstSearch/src/Graph.java
@@ -4,6 +4,7 @@ public class Graph {
 
     private int V;
     private LinkedList<Integer> adjList[];
+    public int count = 0;
 
     public Graph(int v){
         V = v;
@@ -29,24 +30,38 @@ public class Graph {
         visited[v] = true;
         System.out.println(v+" ");
         for(int temp:adjList[v]){
-            System.out.println("i eh "+ temp);
             if(!visited[temp]){
                 depthFirstSearchUtil(temp,visited);
             }
         }
     }
 
+    public void numberOfConnectedComponents(){
+        boolean visited[] = new boolean[V];
+        int x = 0;
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                depthFirstSearchUtil(i,visited);
+                System.out.println("---");
+                x = x + 1;
+            }
+        }
+        System.out.println("O numero de componentes conexos eh: ");
+        System.out.println(x);
 
+    }
 
     public static void main(String args[]){
-        Graph graph = new Graph(100);
+        Graph graph = new Graph(10);
         graph.addAresta(0,1);
         graph.addAresta(0,2);
-        graph.addAresta(2,10);
-        graph.addAresta(10,90);
-        graph.addAresta(0,4);
-        graph.depthFirstSearch(0);
-        
+        graph.addAresta(1,3);
+        graph.addAresta(1,2);
+        graph.addAresta(5,4);
+
+        graph.numberOfConnectedComponents();
+
+
     }
 
 
