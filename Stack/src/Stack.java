c78eb8bb36160a2c82dbb18b84cff3034c32e39b
joaovgatti <joaovsgt@dcc.ufrj.br>
2020-08-23 17:56:30 -0300

    changes in the stack

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,5 +1,14 @@
+/**
+ * This is the implementation of the Stack ADT using a linked-list.
+ * Stack is Lifo - Last in First out - data structure.
+ *
+ * @author João Vitor Gatti Teixeira, joaogatti17@gmail.com
+ */
 public class Stack {
 
+    /**
+     * This is a nested class representing our Node objects.
+     */
     class Node{
         int data;
         Node next;
@@ -8,46 +17,67 @@ public class Stack {
             this.data = data;
         }
     }
-    Node head;
+    private Node head;
 
+    /**
+     *
+     * @param data -> representing the value we want to add in the Stack.
+     *  This method add a new element in the stack as the first element.
+     *   Runs in O(1).
+     */
     public void push(int data){
         Node temp = new Node(data);
-        Node aux = head;
-        if(head == null){
-            head = temp;
+        if (!this.isEmpty()) {
+            head.next = temp;
         }
         head = temp;
-        temp.next = aux;
     }
 
+    /**
+     * This method removes the last element that was add in the Stack.
+     * Runs in O(1).
+     */
     public void pop(){
-        if(head == null){
-            System.out.println("nothing to pop");
-            return;
-        }
-        head = head.next;
+        if(this.isEmpty()){
+            System.out.println("Stack is empty.");
+        }else head = head.next;
     }
 
+    /**
+     *
+     * @return true if the stack is empty.
+     * Runs in O(1).
+     */
     public boolean isEmpty(){
-        if(head == null) return true;
-        return false;
+        return head == null;
     }
 
+
+    /**
+     *
+     * @return the last element that was add in the stack - the element in the top of the stack.
+     * Runs in O(1).
+     */
     public int top(){
-        if(isEmpty() == true){
-            System.out.println("vazia");
-            return Integer.MIN_VALUE;
-        }
-        else{
-            return(head.data);
-        }
+        if(isEmpty()){
+            System.out.println("Stack is empty.");
+            return -1;
+        }else return (head.data);
     }
 
+    /**
+     * This method traversal the elements in the stack printing its values.
+     * Runs in O(n).
+     */
     public void PrintStack(){
-        Node temp = head;
-        while(temp!=null){
-            System.out.println(temp.data);
-            temp  = temp.next;
+        if(this.isEmpty()){
+            System.out.println("Stack is empty.");
+        }else {
+            Node temp = head;
+            while(temp != null){
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
         }
     }
 }
