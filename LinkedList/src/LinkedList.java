6b671114f39646f692094d0cd744229cc0972e58
joaovgatti <joaovsgt@dcc.ufrj.br>
2020-08-23 17:03:11 -0300

    changes in linkedlist

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,5 +1,15 @@
+/**
+* A single linked-list implementation. This is the most basic data structure and we're
+* going to be using it a lot in the upcoming algorithms.
+** @author João Vitor Gatti Teixeira, joaogatti17@gmail.com
+*
+*
+*
+* */
 public class LinkedList {
-
+    /**
+     * This is a nested class for representing an Node Object.
+     */
     class Node{
         int data;
         Node nxt;
@@ -12,11 +22,24 @@ public class LinkedList {
 
     Node head;
 
+    /**
+     *
+     * @param data representing the value you want to add in the linked-list.
+     *  This method add elements as the first element in the linked-list.
+     *  Runs in O(1).
+     */
     public void addFront(int data){
         Node temp = new Node(data);
         temp.nxt = head;
         head = temp;
     }
+
+    /**
+     *
+     * @param data representing the value you want to add in the linked-list.
+     *         This method add elements as the last element in the linked-list.
+     *         Runs in O(n).
+     */
     public void append(int data){
         Node temp = new Node(data);
         Node last = head;
@@ -27,6 +50,10 @@ public class LinkedList {
 
     }
 
+    /**
+     * This method traversal the elements in the linked-list,printing its values.
+     * Runs in O(n).
+     */
     public void printlist(){
         Node tnode = head;
         while(tnode != null){
@@ -35,6 +62,10 @@ public class LinkedList {
         }
     }
 
+    /**
+     * This method remove the last element in the linked-list.
+     * Runs in O(n).
+     */
     public void removelastNode(){
         Node last = head;
         while(last.nxt != null){
@@ -43,9 +74,5 @@ public class LinkedList {
 
 
     }
-
-
-
-
 
 }
